7d10afc45f217620eaa0b10bffd1b4eff61d8f58
Rohit-1717 <rohitvishwakarma1717@gmail.com>
2024-09-08 23:23:41 +0530

    Update dotenv configuration and package.json build script

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import http from "http";
 import cors from "cors";
 import { Server } from "socket.io";
-dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
